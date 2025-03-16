e81f4229430cf4e0ec10549d6d79b814933db2d1
Douglas <contato.cunhadoug@gmail.com>
2025-03-15 23:24:34 -0300

    add about page and about route

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,9 @@
+<script setup>
+    defineProps({
+        name: String,
+    })
+</script>
+
+<template>
+    <h1>About Page {{name}}</h1>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+Route::get('/about', function () {
+   return Inertia::render('About', [
+       'name' => 'Douglas',
+   ]);
+});
